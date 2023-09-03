dd161f652f65e06bb7cf0071822ce5fde3d008d0
kite.lin <kite.lin@blackwellglobal.com>
2023-09-03 23:18:28 +0800

    send email

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,21 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Route;
-use App\Models\User;
-use App\Jobs\KiteJobs;
-use App\Jobs\BatchJobs;
-use Illuminate\Support\Facades\Bus;
-use Illuminate\Bus\Batch;
-use PHPUnit\Event\Code\Throwable;
-use Illuminate\Support\Facades\Cache;
 use App\Events\SomeEvent;
+use App\Http\Controllers\ProfileController;
+use App\Jobs\BatchJobs;
+use App\Jobs\KiteJobs;
+use App\Mail\SendEmail;
 use App\Models\Post;
+use App\Models\User;
+use Illuminate\Bus\Batch;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Route;
+use PHPUnit\Event\Code\Throwable;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -38,16 +41,16 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
-Route::get('jobs/{jobs}',function($jobs){
-   $user = User::find(1);
-   for($i = 0; $i<$jobs;$i++){
-       KiteJobs::dispatch($user);
-   }
-   return 'Jobs processing';
+Route::get('jobs/{jobs}', function ($jobs) {
+    $user = User::find(1);
+    for ($i = 0; $i < $jobs;$i++) {
+        KiteJobs::dispatch($user);
+    }
+    return 'Jobs processing';
 });
-Route::get('batchjobs',function(){
+Route::get('batchjobs', function () {
     $batch = Bus::batch([
         new BatchJobs(User::find(1)),
         new BatchJobs(User::find(2)),
@@ -62,21 +65,20 @@ Route::get('batchjobs',function(){
         // The batch has finished executing...
         Log::info('The batch has finished executing...');
     })->name('My batch of jobs')
-    ->dispatch();;
+    ->dispatch();
 
-    return 'Batch: '. $batch->id . 'is processing';
+    return 'Batch: ' . $batch->id . 'is processing';
 });
 
-
-Route::get('cache',function(){
-    if(Cache::get('user')){
+Route::get('cache', function () {
+    if (Cache::get('user')) {
         return Cache::get('user');
     }
-    Cache::put('user',User::find(1),8);
+    Cache::put('user', User::find(1), 8);
     return 'User cached for 8 seconds';
 });
 
-Route::get('dump',function(){
+Route::get('dump', function () {
     $user1 = User::find(1);
     $user2 = User::find(2);
     dump($user1);
@@ -84,28 +86,49 @@ Route::get('dump',function(){
     return 'Dump complete';
 });
 
-Route::get('events',function(){
+Route::get('events', function () {
     event(new SomeEvent(User::find(1)));
     return 'Event fired.';
 });
 
-Route::get('exceptions',function(){
+Route::get('exceptions', function () {
     throw new Exception('this is error message');
 });
-Route::get('exceptions2',function(){
+Route::get('exceptions2', function () {
     // this one can not caught by telescope
     try {
         throw new Exception('this is error message 2');
-    }catch (Exception $exception){
+    } catch (Exception $exception) {
         Log::info($exception->getMessage());
     }
     return 'error pass';
 });
 
-Route::get('post_first_gate/{post}/edit',function(Post $post,Request $request){
+Route::get('post_first_gate/{post}/edit', function (Post $post, Request $request) {
     return 'you are editing post first gate';
 })->middleware('can:first-gate-update-post,post');
 
-Route::get('post/{post}/edit',function(Post $post,Request $request){
+Route::get('post/{post}/edit', function (Post $post, Request $request) {
     return 'you are editing post second gate';
 })->middleware('can:view,post');
+
+Route::get('logs', function () {
+    Log::emergency('Emergency');
+    Log::alert('Alert');
+    Log::critical('Critical');
+    Log::error('Error');
+    Log::warning('Warning');
+    Log::notice('Notice');
+    Log::info('info');
+    Log::debug('debug');
+    return 'stuff was logged';
+});
+
+Route::get('sendEmail', function () {
+    $data['email'] = 'test@gmail.com';
+    $data['template'] = 'email.test_email';
+    $data['subject'] = 'this is subject';
+    $data['conetent'] = 'this is content data';
+    Mail::to($data['email'])->send(new SendEmail($data));
+    return 'mail sent';
+});
